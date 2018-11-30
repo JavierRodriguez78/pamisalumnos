2c28eee64c07b81b16a34c35d84cf2afedc946b7
Xavi <xavi@geekshubs.com>
2018-11-30 12:47:01 +0100

    finish activate process

--- controllers/auth/activateController.js
+++ controllers/auth/activateController.js
@@ -5,16 +5,20 @@ class activateController extends Controller{
     constructor(req, res,next){
         super(req,res,next);
     }
+
     async activated(){
         let hash = this.req.params.hash;
         let userModel = new UserModel();
         try {
             let resultUser = await userModel.getUserByHash(hash);
-            console.log(resultUser);
-            let result = await userModel.setActiveUser(resultUser[0].id);
-            console.log(result);
+            if(resultUser.length > 0){
+                let result = await userModel.setActiveUser(resultUser[0].id);
+                this.res.render('activate');
+            }else{
+                this.res.render('activate', {error:"El hash no existe"});
+            }
         }catch(error){
-            console.log(error);
+            this.res.render('activate',{error: error});
         }
     }
 }
